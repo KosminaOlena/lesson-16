ba28e967f7d3dd662c43e6e536f767fac59f8eee
KosminaOlena <113665261+KosminaOlena@users.noreply.github.com>
2023-05-23 12:45:16 +0300

    create

---
+++ scripts/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+    addBtn: document.getElementById("addBtn"),
+    myUL: document.getElementById("myUL"),
+    myInput: document.getElementById("myInput")
+};
+
+export default refs;
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+import {addNewTask} from "./functions.js";
+import refs from "./refs.js";
+
+
+refs.addBtn.addEventListener("click", addNewTask)
+// refs.myUL.addEventListener("click")
